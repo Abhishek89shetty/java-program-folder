a184b2e4e5162640cb587280f4e8be47dcebddd8
Abhishek89shetty <abhishek88shettygmail.com>
2026-02-11 21:25:48 +0530

    my first java program

---
+++ Hello.java
@@ -0,0 +1,12 @@
+class Hello
+{
+   
+   public static void main(String[] args)
+{
+     int num1=1,num2=55,num3=45;
+     int sum=num1+num2+num3;
+     
+System.out.println("sum is :"+sum)   ; 
+System.out.print(sum);
+}
+}   
