f45cdc7f4bc8b0f569eb1be5150897dc4d777cbe
Abhishek S Shetty <abhishek88shetty@gmail.com>
2026-02-14 23:16:42 +0530

    Add files via upload

---
+++ Calucate_cgpa.java
@@ -0,0 +1,10 @@
+class Calucate_cgpa
+{
+    public static void main(String[] args)
+    {
+        float kan=67,eng=88,pst=88;
+        System.out.println("the total ur cgpa is:");
+        float cgpa= (kan+eng+pst)/30;
+        System.out.println(cgpa);
+    }
+}
