b5e6516801b2e7964f15f24924126b7bd4f86d4d
Abhishek S Shetty <abhishek88shetty@gmail.com>
2026-03-14 21:09:57 +0530

    it accpect only intger value.

---
+++ Test.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class Test {
+public static void main(String[] args)
+{
+    int a;
+    Scanner ci = new Scanner(System.in);
+    System.out.println("enter the number:");
+    System.out.println(ci.hasNextInt());
+}
+}
